7c6294a17a94ffdc4f7a79c6765175bd366a44ee
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-11-04 13:36:44 -0400

    products findAll test works

--- AuthenticatedBackend/src/test/java/com/magee/repository/ProductRepositoryTest.java
+++ AuthenticatedBackend/src/test/java/com/magee/repository/ProductRepositoryTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @SpringBootTest
 class ProductRepositoryTest {
@@ -26,9 +27,39 @@ class ProductRepositoryTest {
                 .imageName("https://via.placeholder.com/350x250.jpg")
                 .build();
 
-
         productRepository.save(product);
     }
+
+
+    @Test
+    public void getAllProducts() {
+
+        Product product = Product.builder()
+                .productSku("YET-001")
+                .name("Solor Geeks Yeti")
+                .description("Keep Cool all day long!")
+                .price(new BigDecimal("21.99"))
+                .imageName("https://via.placeholder.com/350x250.jpg")
+                .build();
+
+        productRepository.save(product);
+
+
+        List<Product> productList = productRepository.findAll();
+
+        System.out.println("Products are:" + productList);
+
+
+    }
+
+
+
+
+
+
+
+
+
 
 
 
