ca541124324f13b6ab4a2af45bff10f8256bd4ca
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-11-11 14:19:27 -0500

    Working cart item repo

--- AuthenticatedBackend/src/main/java/com/magee/repository/CartItemRepository.java
+++ AuthenticatedBackend/src/main/java/com/magee/repository/CartItemRepository.java
@@ -2,10 +2,27 @@ package com.magee.repository;
 
 import com.magee.models.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+
+    @Query(
+           value = "SELECT * FROM cart_item WHERE cart_item_id = ?1 AND user_id = ?2",
+           nativeQuery = true
+    )
+    CartItem getCartItemByCartItemIdAndUserId(Long cartItemId, Long userId);
+
+
+    @Query(
+            value = "SELECT * FROM cart_item WHERE product_id = ?1 AND user_id = ?2",
+            nativeQuery = true
+    )
+    CartItem getCartItemByProductIdAndUserId(Long productId, Long userId);
+
+
+
 
 
 
