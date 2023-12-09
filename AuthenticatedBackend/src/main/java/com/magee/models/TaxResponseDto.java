1be401cc4124ca422d3c244e58c9d5fd423cc4d6
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-12-09 13:37:19 -0500

    added tax response

---
+++ AuthenticatedBackend/src/main/java/com/magee/models/TaxResponseDto.java
@@ -0,0 +1,31 @@
+package com.magee.models;
+
+import java.math.BigDecimal;
+
+public class TaxResponseDto {
+
+    private BigDecimal salesTax;
+
+    private String lastUpdated;
+
+
+    public BigDecimal getSalesTax() {
+        return salesTax;
+    }
+
+    public void setSalesTax(BigDecimal salesTax) {
+        this.salesTax = salesTax;
+    }
+
+    public String getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public void setLastUpdated(String lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
+
+
+
+
+}
--- AuthenticatedBackend/src/main/java/com/magee/services/TaxService.java
+++ AuthenticatedBackend/src/main/java/com/magee/services/TaxService.java
@@ -1,6 +1,10 @@
 package com.magee.services;
 
+import com.magee.models.TaxResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.math.BigDecimal;
 
@@ -14,7 +18,27 @@ public class TaxService {
 
 
     public BigDecimal getTaxRate(String stateCode) {
-        return null;
+
+        String url = API_BASE_URL + "/statetax?state=" + stateCode.toUpperCase();
+
+        try {
+
+            TaxResponseDto taxResponseDto = restTemplate.getForObject(url, TaxResponseDto.class);
+
+            return taxResponseDto.getSalesTax().divide(new BigDecimal(100));
+        } catch (HttpClientErrorException e) {
+            if (e.getRawStatusCode() == 404) {
+                // tax was not found, throw error
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Tax not found for state '" + stateCode.toUpperCase() + "'. Please check your address");
+            } else {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "There was an error getting the tax rate", e);
+            }
+        }
+
+
+
+
+
     }
 
 
